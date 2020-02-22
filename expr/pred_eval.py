e147557b8d89d9abda99c1c9642ce7b8e81fbb29
jiac <jiac@thunderobot>
2020-02-22 17:45:51 +0800

    sync

--- expr/pred_eval.py
+++ expr/pred_eval.py
@@ -421,7 +421,7 @@ def eval_precision_recall():
   # epoch = 36
   # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_simple_sc_expr', 'tf_resnet152_450.512.512.0.lstm.5.50.5.0.80')
   # epoch = 35
-  pred_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_cider_sc_expr', 'tf_resnet152_450.512.512.0.lstm.mean.5.50.5.0.80.1.0.8.5.0', 'pred')
+  expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_cider_sc_expr', 'tf_resnet152_450.512.512.0.lstm.mean.5.50.5.0.80.1.0.8.5.0')
   epoch = 16
 
   # pred_file = os.path.join(expr_dir, 'pred', '38-beam-50-50-nucleus_sample-0.80-50-sample_topk-5-50.json')
