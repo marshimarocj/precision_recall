30e9365ee3b72bf9e3d1cecc26f1a0fce3580707
jiac <jiac@thunderobot>
2020-02-26 09:52:20 +0800

    sync

--- expr/pred_eval.py
+++ expr/pred_eval.py
@@ -553,16 +553,16 @@ def eval_precision_recall():
 
 
 def eval_precision_only():
-  root_dir = '/data1/jiac/mscoco' # mercurial
+  # root_dir = '/data1/jiac/mscoco' # mercurial
   # root_dir = '/data1/jiac/MSCOCO' # uranus
-  # root_dir = '/hdd/mscoco' # aws
+  root_dir = '/hdd/mscoco' # aws
   # root_dir = '/mnt/data1/jiac/mscoco' # neptune
   gt_file = os.path.join(root_dir, 'aux', 'human_caption_dict.pkl')
 
   # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_ml_expr', 'tf_resnet152_450.512.512.0.lstm')
   # epoch = 38
-  expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_sc_expr', 'tf_resnet152_450.512.512.0.lstm')
-  epoch = 48
+  # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_sc_expr', 'tf_resnet152_450.512.512.0.lstm')
+  # epoch = 48
   # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_rl_expr', 'tf_resnet152_450.512.512.0.0.lstm')
   # epoch = 36
   # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_simple_sc_expr', 'tf_resnet152_450.512.512.0.lstm.5.50.5.0.80')
@@ -577,6 +577,8 @@ def eval_precision_only():
   # epoch = 4
   # expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_cider_sc_expr', 'tf_resnet152_450.512.512.0.lstm.mean.5.50.5.0.80.1.0.8.2.5')
   # epoch = 21
+  expr_dir = os.path.join(root_dir, 'pytorch', 'vevd_gan_cider_sc_expr', 'tf_resnet152_450.512.512.0.lstm.mean.5.50.5.0.80.1.0.8.1.0')
+  epoch = 44
 
   pred_files = [
     os.path.join(expr_dir, 'pred', '%d-beam-100-100.gather.json'%epoch),
@@ -623,9 +625,9 @@ def predict_eval_discriminator():
 
 
 if __name__ == '__main__':
-  predict_eval()
+  # predict_eval()
   # predict_decode()
   # gather_predict_score()
   # eval_precision_recall()
-  # eval_precision_only()
+  eval_precision_only()
   # predict_eval_discriminator()
